6e9f23656c1d68a2218e183788c8f088ac8ffa5e
Talha Siddiqui <talha03182301690@gmail.com>
2026-07-17 21:02:51 +0500

    Time: 0 ms (100%), Space: 11.8 MB (11.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,38 +1,61 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeros = 0;
-        int ones = 0;
-        int twos = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i]==0)
-            {
-                zeros++;
-            }else if (nums[i] == 1)
-            {
-                ones++;
-            }else
-            {
-                twos++;
-            }   
-        }
-    
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (zeros != 0)
-            {
-                nums[i] = 0;
-                zeros--;
-            }else if (ones != 0)
-            {
-                nums[i] = 1;
-                ones--;
-            }else
-            {
-                nums[i] = 2;
+        int mid = 0, low = 0, high = nums.size() - 1;
+
+        while(mid <= high) {
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
             }
-        
+            else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[high], nums[mid]);
+                high--;
+            }
         }
     }
 };
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int zeros = 0;
+//         int ones = 0;
+//         int twos = 0;
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (nums[i]==0)
+//             {
+//                 zeros++;
+//             }else if (nums[i] == 1)
+//             {
+//                 ones++;
+//             }else
+//             {
+//                 twos++;
+//             }   
+//         }
+    
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (zeros != 0)
+//             {
+//                 nums[i] = 0;
+//                 zeros--;
+//             }else if (ones != 0)
+//             {
+//                 nums[i] = 1;
+//                 ones--;
+//             }else
+//             {
+//                 nums[i] = 2;
+//             }
+        
+//         }
+//     }
+// };
+
+
