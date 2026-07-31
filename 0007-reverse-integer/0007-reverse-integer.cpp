eb93c7c98dcf35c6dfd5014659463daf82aaf57e
Talha Siddiqui <talha03182301690@gmail.com>
2026-07-31 18:28:36 +0500

    Time: 5 ms (20.15%), Space: 8.7 MB (21.3%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int reverse(int x) {
-        bool negative = false;
-        long long temp = x;
-
-        if (temp < 0) {
-            negative = true;
-            temp = -temp;
+    int reverse(int n) {
+        int revnum = 0;
+        while(n != 0){
+            int dig = n%10;
+            if (revnum > INT_MAX/10  ||  revnum < INT_MIN/10){
+                return 0;
+            }
+            revnum = revnum *10 +dig;
+            n = n/10;
         }
-
-        string s = to_string(temp);
-        std::reverse(s.begin(), s.end());
-
-        long long num = stoll(s);
-
-        if (negative)
-            num = -num;
-
-        if (num < INT_MIN || num > INT_MAX)
-            return 0;
-
-        return (int)num;
+        return revnum;
     }
 };
