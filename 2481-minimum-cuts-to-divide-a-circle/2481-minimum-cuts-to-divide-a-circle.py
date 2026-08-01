3fab14ca9c3f4e0649e7f8e6476cc03e125c82cf
Talha Siddiqui <talha03182301690@gmail.com>
2026-08-01 13:45:15 +0500

    Time: 0 ms (100%), Space: 19.3 MB (10.53%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if(n%2 ==0):
+            return (n//2)
+        elif(n==1):
+            return 0
+        else:
+            return n
