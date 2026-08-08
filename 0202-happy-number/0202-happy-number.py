4cb88186d366470f6d8d59e991688faaf8af03fe
Talha Siddiqui <talha03182301690@gmail.com>
2026-08-08 12:42:57 +0500

    Time: 3 ms (32.92%), Space: 19.3 MB (61.87%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s=set()
+        while n != 1:
+            if n in s:
+                return False
+                break
+            s.add(n)
+    
+            n = sum(int(i)**2 for i in str(n))
+        return True
