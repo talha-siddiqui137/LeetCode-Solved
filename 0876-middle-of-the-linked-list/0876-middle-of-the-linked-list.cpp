0d1ae24c61951788981b1abf5a934b304a1717d7
Talha Siddiqui <talha03182301690@gmail.com>
2026-08-24 18:19:36 +0500

    Time: 0 ms (100%), Space: 9.8 MB (99.67%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -7,35 +7,53 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* temp = head;
+//         int len = 1;
+//         while(temp != NULL){
+//             len++;
+//             temp = temp->next;
+//         }
+//         int newLen;
+
+//         if(len % 2 == 0){
+//             newLen = len/2 ;
+//         }else{
+//             newLen = len/2 +1;
+//         }
+
+//         temp = head;
+//         int track = 1;
+
+//         while(temp != NULL){
+//             if(track == newLen){
+//                 break;
+//             }else{
+//                 temp = temp->next;
+//                 track++;
+//             }
+//         }
+//         return temp;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int len = 1;
-        while(temp != NULL){
-            len++;
-            temp = temp->next;
-        }
-        int newLen;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        if(len % 2 == 0){
-            newLen = len/2 ;
-        }else{
-            newLen = len/2 +1;
+        while((fast != NULL) && (fast->next != NULL) ){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        temp = head;
-        int track = 1;
-
-        while(temp != NULL){
-            if(track == newLen){
-                break;
-            }else{
-                temp = temp->next;
-                track++;
-            }
-        }
-        return temp;
+        return slow;
     }
 };
