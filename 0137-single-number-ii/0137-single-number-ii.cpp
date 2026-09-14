267ba93d54ffc61470eba39088282c974c85ef9a
Talha Siddiqui <talha03182301690@gmail.com>
2026-09-14 10:09:07 +0500

    Time: 0 ms (100%), Space: 13.4 MB (66.38%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones =0 , twos =0;
+        for(int x: nums)
+        {
+            ones = (ones ^ x) & ~twos;
+            twos = (twos ^ x) & ~ones;
+        }
+        return ones;
+    }
+};
